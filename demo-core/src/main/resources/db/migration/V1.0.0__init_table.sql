df4889ec2176803f8f481127584ad420be263fe4
yaoliansheng <Sd54325000>
2019-11-26 12:34:06 +0800

    加入flyway-core数据库版本控制

---
+++ demo-core/src/main/resources/db/migration/V1.0.0__init_table.sql
@@ -0,0 +1,8 @@
+-- BEGIN 测试表
+CREATE TABLE IF NOT EXISTS `demo` (
+  `id` bigint(20) NOT NULL COMMENT '主键ID',
+  `name` VARCHAR (64)  NOT NULL COMMENT '名称',
+  `mobile_no` VARCHAR (64)  NOT NULL COMMENT '电话',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB  DEFAULT CHARSET=utf8 COMMENT='测试表';
+-- END 测试表
